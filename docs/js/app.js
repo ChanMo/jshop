42331b5024ca1d020fda3293f94bbb455ecf2de3
ChanMo <chen.orange@aliyun.com>
2016-10-19 17:38:37 +0800

    first

---
+++ docs/js/app.js
@@ -0,0 +1,19 @@
+$(function(){
+  $(".fa-chevron-left").parent("a").bind("click", function(e){
+    e.preventDefault();
+    window.history.back();
+  });
+
+  $("[data-off-canvas-toggle]").bind("click", function(e){
+    e.preventDefault();
+    var ele = $(".off-canvas");
+    ele.toggleClass("off-canvas_active");
+    ele.find(".off-canvas__bg").toggle();
+  });
+
+  $("[data-modal-toggle]").bind("click", function(e){
+    e.preventDefault();
+    $(".modal-bg").toggle();
+    $(".modal").toggle();
+  });
+});
